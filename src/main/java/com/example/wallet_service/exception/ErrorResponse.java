af13fd78baa1537eb75313516074ac2b3685f868
Rustam Suleymanov <dev.suleymanov@gmail.com>
2025-11-04 22:48:04 +0300

    clean up code and remove unused dependencies

--- src/main/java/com/example/wallet_service/exception/ErrorResponse.java
+++ src/main/java/com/example/wallet_service/exception/ErrorResponse.java
@@ -1,14 +1,33 @@
 package com.example.wallet_service.exception;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "success", "type", "subtype", "message" })
 public class ErrorResponse {
     private final boolean success = false;
     private final String message;
-    private final ErrorType type;
-    private final Enum<?> subtype;
+    private final String type;
+    private final String subtype;
 
     public ErrorResponse(ErrorType type, Enum<?> subtype, String message) {
-        this.type = type;
-        this.subtype = subtype;
+        this.type = type.name();
+        this.subtype = subtype != null ? subtype.name() : null;
         this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSubtype() {
+        return subtype;
     }
 }
